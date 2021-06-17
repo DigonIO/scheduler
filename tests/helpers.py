981dcfaca74df9aebe82b7acad2bdf8dc21b2bb1
Jendrik A. Potyka <jpotyka@digon.io>
2021-06-17 03:32:09 +0200

    test cyclic jobs; prepare generalized helper.py

--- tests/helpers.py
+++ tests/helpers.py
@@ -2,7 +2,33 @@ import datetime as dt
 
 import pytest
 
+# TODO remove static date from tests and store in this file
+# TODO review the samples and see which ones are no longer needed,
+# need to be adjusted or new ones are needed
+
+utc = dt.timezone.utc
 T_2021_5_26__3_55 = dt.datetime(2021, 5, 26, 3, 55)  # a Wednesday
+T_2021_5_26__3_55_utc = dt.datetime(2021, 5, 26, 3, 55, tzinfo=utc)
+
+CYCLIC_TYPE_ERROR_MSG = (
+    "Wrong input for Cyclic! Select one of the following input types:\n"
+    + "datetime.timedelta | list[datetime.timedelta]"
+)
+_DAILY_TYPE_ERROR_MSG = (
+    "Wrong input for {0}! Select one of the following input types:\n"
+    + "datetime.time | list[datetime.time]"
+)
+MINUTELY_TYPE_ERROR_MSG = _DAILY_TYPE_ERROR_MSG.format("Minutely")
+HOURLY_TYPE_ERROR_MSG = _DAILY_TYPE_ERROR_MSG.format("Hourly")
+DAILY_TYPE_ERROR_MSG = _DAILY_TYPE_ERROR_MSG.format("Daily")
+WEEKLY_TYPE_ERROR_MSG = (
+    "Wrong input for Weekly! Select one of the following input types:\n"
+    + "DAY | list[DAY]\n"
+    + "where `DAY = Weekday | tuple[Weekday, dt.time]`"
+)
+
+_TZ_ERROR_MSG = "Can't use offset-naive and offset-aware datetimes together for {0}."
+TZ_ERROR_MSG = _TZ_ERROR_MSG[:-9] + "."
 
 
 @pytest.fixture
@@ -78,3 +104,11 @@ samples_job_creation = [
     T_2021_5_26__3_55,
     T_2021_5_26__3_55,
 ]
+
+
+def foo(msg="foo"):
+    print(msg)
+
+
+def bar(msg="bar"):
+    print(msg)
--- tests/test_sch_cyclic.py
+++ tests/test_sch_cyclic.py
@@ -2,10 +2,40 @@ import datetime as dt
 
 import pytest
 
-from scheduler import SchedulerError
-from scheduler.job import Job, JobType
+from helpers import (
+    utc,
+    CYCLIC_TYPE_ERROR_MSG,
+    patch_datetime_now,
+    samples_seconds,
+    samples_days,
+    foo,
+)
+
+from scheduler import Scheduler, SchedulerError
+from scheduler.job import Job
 from scheduler.util import Weekday
 
-utc = dt.timezone.utc
-T_2021_5_26__3_55 = dt.datetime(2021, 5, 26, 3, 55)  # a Wednesday
-T_2021_5_26__3_55_utc = dt.datetime(2021, 5, 26, 3, 55, tzinfo=utc)
+
+@pytest.mark.parametrize(
+    "timing, counts, patch_datetime_now, tzinfo, err_msg",
+    (
+        [dt.timedelta(seconds=4), [1, 2, 2, 2, 3, 3, 3], samples_seconds, None, None],
+        [dt.timedelta(seconds=5), [1, 1, 2, 2, 2, 3, 3], samples_seconds, None, None],
+        [dt.timedelta(seconds=5), [1, 1, 2, 2, 2, 3, 3], samples_seconds, utc, None],
+        [dt.timedelta(days=2), [0, 0, 1, 2, 3, 4, 5], samples_days, None, None],
+        [dt.time(hour=2), [], samples_days, None, CYCLIC_TYPE_ERROR_MSG],
+        [Weekday.MONDAY, [], samples_days, None, CYCLIC_TYPE_ERROR_MSG],
+    ),
+    indirect=["patch_datetime_now"],
+)
+def test_cyclic(timing, counts, patch_datetime_now, tzinfo, err_msg):
+    sch = Scheduler(tzinfo=tzinfo)
+
+    if err_msg:
+        with pytest.raises(SchedulerError) as err_msg:
+            job = sch.cyclic(timing=timing, handle=foo)
+    else:
+        job = sch.cyclic(timing=timing, handle=foo)
+        for count in counts:
+            sch.exec_pending_jobs()
+            assert job.attemps == count
