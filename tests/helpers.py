26436b47de420ddb424a63ac2e4a095754c07ae5
Fabian Preiss <fpreiss@digon.io>
2021-07-01 18:19:34 +0200

    improved string formatting

--- tests/helpers.py
+++ tests/helpers.py
@@ -251,7 +251,10 @@ job_reprs = (
     ],
     [
         "scheduler.Job(<JobType.MINUTELY: 2>, datetime.time(0, 0, 20), <function bar at 0x",
-        ">, {'msg': 'foobar'}, 20, 0, False, datetime.datetime(2021, 5, 26, 3, 54, 15), datetime.datetime(2021, 5, 26, 4, 5), False, None)",
+        (
+            ">, {'msg': 'foobar'}, 20, 0, False, datetime.datetime(2021, 5, 26, 3, 54, 15),"
+            " datetime.datetime(2021, 5, 26, 4, 5), False, None)"
+        ),
     ],
     [
         "scheduler.Job(<JobType.DAILY: 4>, datetime.time(7, 5), <function foo at 0x",
@@ -327,21 +330,27 @@ job_reprs_utc = (
     ],
     [
         (
-            "scheduler.Job(<JobType.HOURLY: 3>, datetime.time(7, 5, tzinfo=datetime.timezone.utc), <built-in function print>"
-            ", {}, 0, 20, False, datetime.datetime(2021, 5, 26, 3, 55, tzinfo=datetime.timezone.utc),"
-            " datetime.datetime(2021, 5, 26, 23, 55, tzinfo=datetime.timezone.utc), False, datetime.timezone.utc)"
+            "scheduler.Job(<JobType.HOURLY: 3>, datetime.time(7, 5, tzinfo=datetime.timezone.utc),"
+            " <built-in function print>, {}, 0, 20, False, datetime.datetime(2021, 5, 26, 3, 55,"
+            " tzinfo=datetime.timezone.utc), datetime.datetime(2021, 5, 26, 23, 55, "
+            "tzinfo=datetime.timezone.utc), False, datetime.timezone.utc)"
         )
     ],
     [
         "scheduler.Job(<JobType.WEEKLY: 5>, <Weekday.MONDAY: 0>, <function bar at 0x",
-        ">, {}, 0, 1, False, datetime.datetime(2021, 5, 25, 3, 55, tzinfo=datetime.timezone.utc), None, True, datetime.timezone.utc)",
+        (
+            ">, {}, 0, 1, False, datetime.datetime(2021, 5, 25, 3, 55, "
+            "tzinfo=datetime.timezone.utc), None, True, datetime.timezone.utc)"
+        ),
     ],
     [
         (
             "scheduler.Job(<JobType.WEEKLY: 5>, [<Weekday.WEDNESDAY: 2>, (<Weekday.TUESDAY: 1>,"
-            " datetime.time(23, 45, 59, tzinfo=datetime.timezone.utc))], <built-in function print>,"
-            " {'end': 'FOO\\n'}, 1, 1, True, datetime.datetime(2021, 6, 2, 3, 55, tzinfo=datetime.timezone.utc),"
-            " datetime.datetime(2021, 7, 25, 3, 55, tzinfo=datetime.timezone.utc), False, datetime.timezone.utc)"
+            " datetime.time(23, 45, 59, tzinfo=datetime.timezone.utc))], <built-in function print>"
+            ", {'end': 'FOO\\n'}, 1, 1, True, "
+            "datetime.datetime(2021, 6, 2, 3, 55, tzinfo=datetime.timezone.utc),"
+            " datetime.datetime(2021, 7, 25, 3, 55, tzinfo=datetime.timezone.utc),"
+            " False, datetime.timezone.utc)"
         )
     ],
 )
