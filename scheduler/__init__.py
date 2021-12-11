3041a6d8cd5c4306676249f24358601ac1359a3a
Jendrik A. Potyka <jpotyka@digon.io>
2021-12-11 23:41:49 +0100

    version bump 0.7.0

--- scheduler/__init__.py
+++ scheduler/__init__.py
@@ -4,7 +4,7 @@ A simple in-process python `scheduler` library for `datetime` objects.
 Author: Jendrik A. Potyka, Fabian A. Preiss
 """
 
-__version__ = "0.6.2"
+__version__ = "0.7.0"
 __author__ = "Jendrik A. Potyka, Fabian A. Preiss"
 
 from scheduler.core import Scheduler
